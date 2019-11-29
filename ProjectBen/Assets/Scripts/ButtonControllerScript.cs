58bf46f05dfc1a2452582dc3095d29dc60691f5c
Michael PUlis <mikeypulis@gmail.com>
2019-11-29 23:19:42 +0100

    First Draft Skill Selector Scene
    : )

---
+++ ProjectBen/Assets/Scripts/ButtonControllerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ButtonControllerScript : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI title;
+    public TMPro.TextMeshProUGUI description;
+
+
+    public void SetCurrent(string n)
+    {
+        title.text = GetTitle(n);
+        description.text = GetTitle(n);
+        Debug.Log("Set");
+    }
+
+    string GetTitle(string n)
+    {
+        switch (n)
+        {
+            case "level1":
+                return "Damage Resistance";
+
+            default:
+                return "Testing Value";
+        }
+    }
+
+    string GetDescription(string n)
+    {
+        switch (n)
+        {
+            case "level1":
+                return "This item increases the amount of damage the player can withstand before dying";
+
+            default:
+                return "Lorem ipsum bla bla bla bla bal bla bla bla bla bal bla bla bla bla bal";
+        }
+    }
+}
