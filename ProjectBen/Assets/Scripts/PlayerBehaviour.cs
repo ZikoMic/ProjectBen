900efcd9133222340a2943999dd0536e31dcb81a
Zak <avatarziko619@gmail.com>
2019-11-30 01:07:20 +0100

    Added Level 1
    Has Spikes and Fall Mechanism

---
+++ ProjectBen/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+using System.Linq; 
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool dead = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider){
+        string[] deathObjects = {"Spikes", "DeathBox"};
+        
+        if(deathObjects.Contains(collider.gameObject.tag) )
+        {
+            dead = true; 
+        }
+    }
+
+    private void Update()
+    {
+        if (dead)
+        {
+            Destroy (GameObject.FindWithTag("Player")); 
+        }
+    }
+}
