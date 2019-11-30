e15fa1e43584fe9b4da3a4440fad04edb73d627b
Zak <avatarziko619@gmail.com>
2019-11-30 04:18:13 +0100

    Added Ladder Prefab
    Has Inertia Bug

---
+++ ProjectBen/Assets/Scripts/LadderZone.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderZone : MonoBehaviour
+{
+    private PlayerBehaviour player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerBehaviour>();
+
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.name == "Player")
+        {
+            player.onLadder = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.name == "Player")
+        {
+            player.onLadder = false;
+        }
+    }
+}
--- ProjectBen/Assets/Scripts/PlayerBehaviour.cs
+++ ProjectBen/Assets/Scripts/PlayerBehaviour.cs
@@ -3,32 +3,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.PlayerLoop;
-using System.Linq; 
+using System.Linq;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    public float climbSpeed = 10f;
     private Rigidbody2D rb;
     private bool dead = false;
+    private float distance;
+    
+    public bool onLadder;
+    private float defaultGravity;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        defaultGravity = rb.gravityScale;
     }
 
-    void OnTriggerEnter2D(Collider2D collider){
+    void OnTriggerEnter2D(Collider2D collider)
+    {
         string[] deathObjects = {"Spikes", "DeathBox"};
-        
-        if(deathObjects.Contains(collider.gameObject.tag) )
+
+        if (deathObjects.Contains(collider.gameObject.tag))
         {
-            dead = true; 
+            dead = true;
         }
     }
 
     private void Update()
     {
+        if (onLadder && Input.GetKey(KeyCode.W))
+        {
+            rb.gravityScale = 0f;
+            rb.velocity = new Vector2(rb.velocity.x, climbSpeed);
+        }
+        
+        if (!onLadder)
+        {
+            rb.gravityScale = defaultGravity;
+        }
+        
         if (dead)
         {
-            Destroy (GameObject.FindWithTag("Player")); 
+            Destroy(GameObject.FindWithTag("Player"));
         }
     }
 }
